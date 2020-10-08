f5ab2d232830376ca8abcefac067c85cc0bbe81d
tisunmaza555 <68525388+tisunmaza555@users.noreply.github.com>
2020-10-08 17:09:24 +0700

    poompamM

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="10">
+      <div class="text-center">
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to my music room by Itunes .If you ready you can push botton
+          continue.
+        </v-card-title>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+export default {
+  components: {
+    VuetifyLogo,
+  },
+}
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: 'https://imgur.com/i2O98d3.jpg',
+        },
+        {
+          src: 'https://imgur.com/xYfgSnq.jpg',
+        },
+        {
+          src: 'https://imgur.com/YrXCSMG.jpg',
+        },
+        {
+          src: 'https://imgur.com/Cke7e5B.jpg',
+        },
+      ],
+    }
+  },
+}
+</script>
+<style></style>
